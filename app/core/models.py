777b001e6feabd8afa8717df766db5708572df35
Shashi7083 <shashikr12047@gmail.com>
2026-02-12 18:33:23 +0530

    admin page customization

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,49 @@
-from django.db import models  # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create , save and return a new user."""
+        if not email:
+            raise ValueError('User must have email.')
+        normalized_email = self.normalize_email(email)
+        user = self.model(email=normalized_email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create, save and return superuser"""
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    #  provide custom fields
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    #  is_staff is used to determine the user can login to Django admin
+    is_staff = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    #  defines field used for authentication
+    USERNAME_FIELD = "email"
